1ec9e8d0da9858872b1a93146542b735dde8b695
St0rage <daniyudistira25@gmail.com>
2021-04-07 10:23:49 +0700

    menambah fitur hover box market-place dan refactor

--- script/products.js
+++ script/products.js
@@ -1,6 +1,20 @@
 // ANIMASI HOVER BOX
-// let products = document.querySelector('.list-products');
 let hoverProducts = document.querySelector('#hover-box');
+let hoverMarketPlace = document.querySelector('#hover-box2');
 function hoverBox() {
+    hoverMarketPlace.classList.remove('hover-market-place');
     hoverProducts.classList.toggle('hover-products');
 }
+function hoverBox2() {
+    hoverProducts.classList.remove('hover-products');
+    hoverMarketPlace.classList.toggle('hover-market-place');
+}
+
+let hoverLi = Array.from(document.querySelector('.list').firstElementChild.children);
+let listImg = document.querySelector('.list-img').firstElementChild;
+hoverLi.forEach(e => {
+    e.addEventListener('mouseover', () => {
+        let text = e.firstElementChild.textContent.toLocaleLowerCase();
+        listImg.setAttribute('src', `img/items/${text}.png`);
+    });
+})
